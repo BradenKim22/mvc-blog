1dc2a5a2c3931e67576f80a877fae931181720ea
BradenKim22 <118400033+BradenKim22@users.noreply.github.com>
2023-04-29 21:51:55 -0600

    Added routes, similar to the other project

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,1 +1,21 @@
 // Comment Routes
+const router = require('express').Router()
+const { Comment } = require('../../models/Comment')
+const withAuth = require('../../utils/auth')
+
+// Post Comment to DB
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.create({
+            contents: req.body.contents,
+            // date_created: req.body.date_created,
+            user_id: req.session.user_id,
+            blog_id: req.body.blog_id
+        })
+        res.status(200).json(commentData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+module.exports = router
