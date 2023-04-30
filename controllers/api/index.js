b2badc0eb3e29c0ffb9e4113638860d16403f57f
BradenKim22 <118400033+BradenKim22@users.noreply.github.com>
2023-04-29 18:08:27 -0600

    Models seem good, added more folder/file structure

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,12 @@
-// 
+// in the /api
+const router = require('express').Router();
+
+const blogRoutes = require('./blogRoutes');
+const userRoutes = require('./userRoutes');
+const commentRoutes = require('./commentRoutes');
+
+router.use('/blog', blogRoutes);
+router.use('/user', userRoutes);
+router.use('/comment', commentRoutes);
+
+module.exports = router;
